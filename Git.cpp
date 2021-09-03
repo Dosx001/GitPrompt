a50e52757f00e231bfdac5851ae50fce2681c25f
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-09-02 19:38:39 -0700

    Fixed bugs

--- Git.cpp
+++ Git.cpp
@@ -1,123 +1,126 @@
 #include <iostream>
 using namespace std;
-void print(char* array, int i) {
-    while (array[i] != '\n') {
-        cout << array[i];
-        i++;
-    }
-}
+void color(char* buffer);
+void print(char* array, int i);
+
 int main() {
     const int max_buffer = 128;
     char buffer[max_buffer];
     if (fgets(buffer, max_buffer, popen("git branch --show-current 2> /dev/null", "r"))) {
         FILE * status;
-        status = popen("git stash list 2> /dev/null", "r");
+        status = popen("git stash list", "r");
         int i = 0;
         while (fgets(buffer, max_buffer, status)) i++;
         if (0 < i) cout << "\033[35mStashes: " << i;
-        cout << "\033[90m\n" << fgets(buffer, max_buffer, popen("git log -1 --pretty=format:'%s' 2> /dev/null", "r")) << '\n';
-        bool check = false;
-        status = popen("git status -s 2> /dev/null", "r");
+        if (fgets(buffer, max_buffer, popen("git log -1 --pretty=format:'%s' 2> /dev/null", "r")))
+            cout << "\033[90m\n" << buffer;
+        status = popen("git status -s", "r");
+        if (fgets(buffer, max_buffer, status)) {
+            cout << "\n\033";
+            fputs(buffer, status);
+            color(buffer);
+        }
         while (fgets(buffer, max_buffer, status)) {
             fputs(buffer, status);
-            switch(buffer[0]) {
-                case '?':
-                    cout << "\033[37m";
-                    break;
-                case ' ':
-                    switch(buffer[1]) {
-                        case 'M':
-                            cout << "\033[33m";
-                            break;
-                        case 'D':
-                            cout << "\033[31m";
-                    }
-                    break;
-                case 'A':
-                    switch(buffer[1]) {
-                        case ' ':
-                            cout << "\033[34m";
-                            break;
-                        case 'A':
-                            cout << "\033[37;44m";
-                            break;
-                        case 'D':
-                            cout << "\033[96m";
-                            break;
-                        case 'M':
-                            cout << "\033[94m";
-                            break;
-                        case 'U':
-                            cout << "\033[30;44m";
-                    }
-                    break;
-                case 'D':
-                    switch(buffer[1]) {
-                        case ' ':
-                            cout << "\033[91m";
-                            break;
-                        case 'D':
-                            cout << "\033[37;41m";
-                            break;
-                        case 'U':
-                            cout << "\033[30;41m";
-                    }
-                    break;
-                case 'M':
-                    switch(buffer[1]) {
-                        case ' ':
-                            cout << "\033[32m";
-                            break;
-                        case 'D':
-                            cout << "\033[38;5;202m";
-                            break;
-                        case 'M':
-                            cout << "\033[93m";
-                    }
-                    break;
-                case 'U':
-                    switch(buffer[1]) {
-                        case 'A':
-                            cout << "\033[33;44m";
-                            break;
-                        case 'D':
-                            cout << "\033[33;41m";
-                            break;
-                        case 'U':
-                            cout << "\033[30;43m";
-                    }
-                    break;
-                case 'R':
-                    switch(buffer[1]) {
-                        case ' ':
-                            cout << "\033[35m";
-                            break;
-                        case 'D':
-                            cout << "\033[38;5;201m";
-                            break;
-                        case 'M':
-                            cout << "\033[38;5;93m";
-                    }
-            }
-            print(buffer, 3);
-            cout << "\033[00m ";
-            check = true;
+            color(buffer);
         }
-        if (check) cout << '\n';
-        fgets(buffer, max_buffer, popen("basename $PWD", "r"));
-        cout << "\033[32;44m";
+        fgets(buffer, max_buffer, popen("git branch --show-current", "r"));
+        cout << "\n\033[30;41m";
         print(buffer, 0);
-        cout << "\033[34;41m\033[30;41m";
-        fgets(buffer, max_buffer, popen("git branch --show-current 2> /dev/null", "r"));
-        print(buffer, 0);
-        cout << "\033[00m\033[31m\033[00m";
+        cout << "\033[31;44m";
         pclose(status);
     }
-    else {
-        fgets(buffer, max_buffer, popen("basename $PWD", "r"));
-        cout << "\033[32;44m\n";
-        print(buffer, 0);
-        cout << "\033[00m\033[34m\033[00m";
-    }
+    else cout << "\n\033";
     return 0;
+}
+
+void color(char* buffer) {
+    switch(buffer[0]) {
+        case '?':
+            cout << "\033[37m";
+            break;
+        case ' ':
+            switch(buffer[1]) {
+                case 'M':
+                    cout << "\033[33m";
+                    break;
+                case 'D':
+                    cout << "\033[31m";
+            }
+            break;
+        case 'A':
+            switch(buffer[1]) {
+                case ' ':
+                    cout << "\033[34m";
+                    break;
+                case 'A':
+                    cout << "\033[37;44m";
+                    break;
+                case 'D':
+                    cout << "\033[96m";
+                    break;
+                case 'M':
+                    cout << "\033[94m";
+                    break;
+                case 'U':
+                    cout << "\033[30;44m";
+            }
+            break;
+        case 'D':
+            switch(buffer[1]) {
+                case ' ':
+                    cout << "\033[91m";
+                    break;
+                case 'D':
+                    cout << "\033[37;41m";
+                    break;
+                case 'U':
+                    cout << "\033[30;41m";
+            }
+            break;
+        case 'M':
+            switch(buffer[1]) {
+                case ' ':
+                    cout << "\033[32m";
+                    break;
+                case 'D':
+                    cout << "\033[38;5;202m";
+                    break;
+                case 'M':
+                    cout << "\033[93m";
+            }
+            break;
+        case 'U':
+            switch(buffer[1]) {
+                case 'A':
+                    cout << "\033[33;44m";
+                    break;
+                case 'D':
+                    cout << "\033[33;41m";
+                    break;
+                case 'U':
+                    cout << "\033[30;43m";
+            }
+            break;
+        case 'R':
+            switch(buffer[1]) {
+                case ' ':
+                    cout << "\033[35m";
+                    break;
+                case 'D':
+                    cout << "\033[38;5;201m";
+                    break;
+                case 'M':
+                    cout << "\033[38;5;93m";
+            }
+    }
+    print(buffer, 3);
+    cout << "\033[00m ";
+}
+
+void print(char* array, int i) {
+    while (array[i] != '\n') {
+        cout << array[i];
+        i++;
+    }
 }
