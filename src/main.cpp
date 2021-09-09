e0bef3c2e7ad3971a0dee8a54a4dc573b64bca61
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-09-09 10:33:50 -0700

    Improved cout speed

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "Git.hpp"
 
 int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
     Git* it = new Git();
     if (it -> start()) {
         std::thread th1(&Git::stash, it);
